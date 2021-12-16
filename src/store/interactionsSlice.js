ee3ce53d0722d82ef4bee987e96acb266c16b58e
Othior <splingaire.marvyn@gmail.com>
2021-12-16 16:31:50 +0100

    test reload page

--- src/store/interactionsSlice.js
+++ src/store/interactionsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import   React,{ useNavigate }from "react"
 
 export const interactionsSlice = createSlice({
     name: 'interactions',
@@ -8,7 +9,8 @@ export const interactionsSlice = createSlice({
         },
         confirmation: {
             open: false,
-        }
+        },
+        navigate: useNavigate()
     },
     reducers: {
         showToast: (state, {payload}) =>  {
@@ -19,6 +21,7 @@ export const interactionsSlice = createSlice({
         },
         askConfirmation: (state, {payload}) => {
             state.confirmation = { open: true, ...payload }
+            useNavigate('participant-list')
         },
         hideConfirmation: (state, {payload}) =>  {
             state.confirmation = { open: false }
